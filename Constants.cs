273fabf2b401bbe6dc53de43a39c0cc87a0bfc91
ZlothY29IQ <211057194+ZlothY29IQ@users.noreply.github.com>
2026-05-18 02:46:13 +0100

    Bump version to v2.0.0

--- Constants.cs
+++ Constants.cs
@@ -1,10 +1,10 @@
 ﻿namespace MonkeRealism
 {
-    internal class Constants
+    internal abstract class Constants
     {
         public const string Guid    = "zlothy.monkerealism";
         public const string Name    = "MonkeRealism";
         public const string HashKey = "github.com/ZlothY29IQ/MonkeRealism";
-        public const string Version = "1.0.0";
+        public const string Version = "2.0.0";
     }
 }
